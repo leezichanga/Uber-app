d6dd52f5ec22fb3872697d16d99615adc1ad256c
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-20 13:14:42 +0300

    create passenger profile in views

--- passenger/views.py
+++ passenger/views.py
@@ -6,3 +6,14 @@ def home(request):
 
     title = 'Passenger'
     return render(request, 'passenger/home.html', {'title':title})
+
+def profile(request, username):
+    '''
+    render passenger information
+    '''
+    user = User.objects.get(username=username)
+    profile = PassengerProfile.objects.get(user=user)
+    location = Location.objects.get(user=user)
+
+    title = f"{user.username}"
+    return render(request, 'passenger/profile.html', {"user":user, "profile":profile, "location":location})
