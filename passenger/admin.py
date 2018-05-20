eba48f7de55856a2993f7f857c9871d5e8fd93f0
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-20 17:34:39 +0300

    register passenger models in admin file

--- passenger/admin.py
+++ passenger/admin.py
@@ -1,1 +1,7 @@
+from django.contrib import admin
+from .models import PassengerProfile, Location, Reviews
 
+# Register your models here.
+admin.site.register(PassengerProfile)
+admin.site.register(Location)
+admin.site.register(Reviews)
