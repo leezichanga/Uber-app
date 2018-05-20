798a8ae063e2805d61df68c77089daa08ae9f043
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-20 14:36:48 +0300

    create find driver url

--- passenger/urls.py
+++ passenger/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^home/$', views.home, name='home'),
     url(r'^passenger_profile/(?P<username>[-_\w.]+)$', views.profile, name='profile'),
+    url(r'^passenger_profile/(?P<username>[-_\w.]+)/update/$', views.update_profile, name='update_profile'),
+    url(r'^drivers/$', views.find_driver, name='find_driver'),
     ]
