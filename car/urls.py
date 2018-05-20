82c7577f0b2f03c608d514314615e3da45947b07
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-20 17:45:43 +0300

    install django registration

--- car/urls.py
+++ car/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls.static import static
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views
-from . import views as carpool_views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
