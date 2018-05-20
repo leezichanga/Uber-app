941fa4db83b3a4f1613bc16297196de2df038fba
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-20 17:03:00 +0300

    add driver profile model

--- driver/models.py
+++ driver/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+class DriverProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to = 'profile/driver/', default='/media/profile/driver/user.svg', null=True)
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True)
+    car_capacity = models.IntegerField(null=True)
+    plates = models.CharField(max_length=30, null=True)
+    car_color = models.CharField(max_length=30, null=True)
+
+    def getPic(self):
+        if not self.profile_pic:
+            return '/media/profile/driver/user.svg'
+
+    # link to in-built user model
+    @receiver(post_save,sender = User)
+    def create_user_profile(sender,instance,created, **kwargs):
+        if created:
+            DriverProfile.objects.create(user=instance)
+
+    @receiver(post_save,sender = User)
+    def save_user_profile(sender,instance,**kwargs):
+        instance.driverprofile.save()
+    # end of link
--- driver/urls.py
+++ driver/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^(?P<username>[-_\w.]+)$', views.home, name='home'),
     url(r'^profile/(?P<username>[-_\w.]+)$', views.profile, name='profile_view'),
+    url(r'^profile/(?P<username>[-_\w.]+)/update/$', views.update_profile, name='update_profile_view'),
+    url(r'^location/(?P<username>[-_\w.]+)/update/$', views.update_location, name='update_location_view'),
+    url(r'^find_passenger/$', views.find_passenger, name='find_passenger'),
     ]
