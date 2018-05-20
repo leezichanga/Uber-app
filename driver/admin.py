41cbb9a397be59f6df41aacab32901e6af7a2f21
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-20 17:33:42 +0300

    register models in driver admin app

--- driver/admin.py
+++ driver/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import DriverProfile, CarPool, Location, Reviews
 
 # Register your models here.
+
+admin.site.register(DriverProfile)
+admin.site.register(CarPool)
+admin.site.register(Location)
+admin.site.register(Reviews)
