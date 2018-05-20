aaa0cc59778b73556cb6f5c4d538e85f53a3c1a7
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-20 16:23:11 +0300

    create home function for driver

--- driver/views.py
+++ driver/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request, username):
+    user = User.objects.get(username=username)
+    driver_profile = DriverProfile.objects.get(user=user)
+    carpool = CarPool.objects.filter(id=user.id)
+    num_passengers = len(carpool)
+    capacity = driver_profile.car_capacity
+    print(carpool)
+    print(num_passengers)
+    if request.method == 'POST':
+        carpool_form = CarPoolForm(request.POST)
+
+        if carpool_form.is_valid() and num_passengers <= capacity:
+            carpool_form.save()
+            messages.success(request, ('You have a ride with this driver!'))
+            return HttpResponseRedirect("/driver/%s"%user.username)
+        else:
+            messages.error(request, ('This car is full, try another driver.'))
+    else:
+        carpool_form = CarPoolForm()
+
+    title = 'Driver'
+    return render(request, 'driver/home.html', {'title':title, "user":user, "carpool":carpool, "form":carpool_form})
