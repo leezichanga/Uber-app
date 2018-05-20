c0edfc648094cce46f2763928e639a97fab60a1a
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-20 13:53:17 +0300

    create update profile view

--- passenger/views.py
+++ passenger/views.py
@@ -17,3 +17,23 @@ def profile(request, username):
 
     title = f"{user.username}"
     return render(request, 'passenger/profile.html', {"user":user, "profile":profile, "location":location})
+
+def update_profile(request, username):
+    user = User.objects.get(username = username)
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = PassengerProfileForm(request.POST, instance=request.user.passengerprofile, files=request.FILES)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, ('Your profile was successfully updated!'))
+            return HttpResponseRedirect("/passenger/passenger_profile/%s"%user.username)
+        else:
+            messages.error(request, ('Please correct the error below.'))
+    else:
+        user_form = UserForm(instance = request.user)
+        profile_form = PassengerProfileForm(instance = request.user.passengerprofile)
+
+    title = 'Update Profile'
+    return render(request, 'passenger/update_profile.html', {"title":title, "user_form": user_form, "profile_form": profile_form})
