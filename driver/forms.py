3b7ad11219e3b7a31facb6b0eea8b6a06101c5dc
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-20 17:27:34 +0300

    create user and driver profile form

--- driver/forms.py
+++ driver/forms.py
@@ -1,3 +1,13 @@
 from django import forms
 from django.contrib.auth.models import User
 from .models import DriverProfile, CarPool, Location, Reviews
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+
+class DriverProfileForm(forms.ModelForm):
+    class Meta:
+        model = DriverProfile
+        fields = ['profile_pic', 'gender', 'car_capacity', 'plates', 'car_color']
