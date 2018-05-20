546296fd6c8305a8d640a14cf7ca0f75e7237491
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-20 14:45:01 +0300

    review driver url

--- passenger/urls.py
+++ passenger/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'^passenger_profile/(?P<username>[-_\w.]+)$', views.profile, name='profile'),
     url(r'^passenger_profile/(?P<username>[-_\w.]+)/update/$', views.update_profile, name='update_profile'),
     url(r'^drivers/$', views.find_driver, name='find_driver'),
+    url(r'^driver/profile/(\d+)$', views.driver_profile, name='driver_profile'),
+    url(r'^driver/(\d+)/review/$', views.review_driver, name='review_driver'),
     ]
