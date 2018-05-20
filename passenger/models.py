a6a7c4fb6cd5132cad5d6fe56c469b6f496e64a6
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-20 15:56:14 +0300

    passenger profile model

--- passenger/models.py
+++ passenger/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class PassengerProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True)
+    profile_pic = models.ImageField(upload_to='profile/passenger/', default='/media/profile/passenger/user.svg', null=True)
+    age = models.IntegerField(null=True)
+
+    def getPic(self):
+        if not self.profile_pic:
+            return '/media/profile/driver/user.svg'
+
+    # link to in-built user model
+    @receiver(post_save,sender = User)
+    def create_user_profile(sender,instance,created, **kwargs):
+        if created:
+            PassengerProfile.objects.create(user=instance)
+
+    @receiver(post_save,sender = User)
+    def save_user_profile(sender,instance,**kwargs):
+        instance.passengerprofile.save()
+    # end of link
