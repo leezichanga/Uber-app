7914b6f5133b787bf19380566ccc53c44824aab2
Elizabeth Wangui <elizabbethichanga@yahoo.com>
2018-05-23 09:13:57 +0300

    update urls

--- driver/urls.py
+++ driver/urls.py
@@ -1,11 +1,14 @@
 from django.conf import settings
 from django.conf.urls import url
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^(?P<username>[-_\w.]+)$', views.home, name='home'),
-    url(r'^profile/(?P<username>[-_\w.]+)$', views.profile, name='profile_view'),
-    url(r'^profile/(?P<username>[-_\w.]+)/update/$', views.update_profile, name='update_profile_view'),
-    url(r'^location/(?P<username>[-_\w.]+)/update/$', views.update_location, name='update_location_view'),
+    url(r'^$', views.home, name='home'),
+    url(r'^profile/$', views.profile, name='profile_view'),
+    url(r'^profile/update/$', views.update_profile, name='update_profile_view'),
+    url(r'^location/update/$', views.update_location, name='update_location_view'),
     url(r'^find_passenger/$', views.find_passenger, name='find_passenger'),
     ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- passenger/urls.py
+++ passenger/urls.py
@@ -1,13 +1,16 @@
 from django.conf import settings
 from django.conf.urls import url
 from . import views
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     url(r'^home/$', views.home, name='home'),
-    url(r'^passenger_profile/(?P<username>[-_\w.]+)$', views.profile, name='profile'),
-    url(r'^passenger_profile/(?P<username>[-_\w.]+)/update/$', views.update_profile, name='update_profile'),
+    url(r'^passenger_profile/$', views.profile, name='profile'),
+    url(r'^passenger_profile/update$', views.update_profile, name='update_profile'),
     url(r'^drivers/$', views.find_driver, name='find_driver'),
     url(r'^driver/profile/(\d+)$', views.driver_profile, name='driver_profile'),
     url(r'^driver/(\d+)/review/$', views.review_driver, name='review_driver'),
     ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
